8d212229aabac7d107e93a9f17313dfd8e02a9b8
Blazoned <bryankroon@ziggo.nl>
2018-04-04 14:01:13 +0200

    Basic ASP.NET Core Setup

---
+++ Acknowledge.PDFDownloader.POP/Acknowledge.PDFDownloader.POP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Acknowledge.PDFDownloader.POP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
